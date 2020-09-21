6b9254453463f3b14b2d07704e9ab9d4247b6abf
Javier Fernández Peón <javierfp@iessanclemente.net>
2020-09-21 21:06:30 +0200

    Commit inicial

---
+++ README.md
@@ -0,0 +1,20 @@
+# Despliegue Odoo
+
+## Objetivo
+
+Con este repositorio se dispondrá de varios escenarios para practicar el acceso a la base de datos de Odoo
+
+## Pasos previos a realizar
+
+La información con los pasos previos y requisitos del entorno de prácticas se encuentran en:
+
+[REQUISITOS](https://github.com/javierfp-isc/sxe_requisitos)
+
+## Escenarios
+
+El escenario es el conjunto de elementos necesarios para la realización de la práctica. Consistirá en un conjunto de contenedores docker, uno o más, con funciones bien establecidas y descritas en la práctica. Tendrás que realizar los pasos indicados en la práctica en ellos, es decir serán la base de trabajo.
+
+Este repositorio contiene los escenarios:
+
+- **odoo_1container**: despliegue completo de odoo en 1 container
+
---
+++ odoo_1container/odoo/entrypoint.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+#Arrancamos servicios
+service ssh start
+service postgresql start
+
+#Movemos el archivo de configuración para que sea accesible desde el anfitrión
+mv /opt/odoo/odoo.conf /opt/odoo/conf/odoo.conf
+
+#Creamos el archivo de log y cambiamos propietario
+touch /var/log/odoo/odoo.log
+chown odoo /var/log/odoo/odoo.log
+
+#Crea el usuario odoo en postgres
+su - postgres -c "createuser --createdb $ODOOUSER" && su - postgres -c "psql -c \"alter role $ODOOUSER with password '$ODOOPASS'\""
+
+#Escribe en /etc/hosts para que resuelva postgres al localhost
+echo "127.0.0.1 postgres" >> /etc/hosts
+
+#Arranca odoo (while: la primera vez no arranca bien)
+while [[ ! $(service odoo.sh start) ]];do continue;done
+
+#Uso exec para lanzar un proceso independiente de bucle infinito
+exec bash -c "while true;do sleep 10;done"
+
