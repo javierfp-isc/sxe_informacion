f26286b3dd49fa62cc5636b6d1c942bacf3d9f93
Javier Fernández Peón <javierfp@iessanclemente.net>
2020-09-24 17:27:24 +0200

    reubicado odoo.conf

--- odoo_bbdd/odoo/entrypoint.sh
+++ odoo_bbdd/odoo/entrypoint.sh
@@ -4,9 +4,6 @@
 service ssh start
 service postgresql start
 
-#Movemos el archivo de configuración para que sea accesible desde el anfitrión
-mv /opt/odoo/odoo.conf /opt/odoo/conf/odoo.conf
-
 #Creamos el archivo de log y cambiamos propietario
 touch /var/log/odoo/odoo.log
 chown odoo /var/log/odoo/odoo.log
