fad1135d5f5b7a2bf4e7bc38c0851e59265e3ff7
Javier Fernández Peón <javierfp@iessanclemente.net>
2020-10-03 17:59:54 +0200

    Commit inicial

--- odoo_replication/postgres/entrypoint.sh
+++ odoo_replication/postgres/entrypoint.sh
@@ -8,22 +8,10 @@ ODOOPASS=abc123.
 
 #Arrancamos servicios
 service ssh start
-service postgresql start
 
-#Ejecutamos en el MASTER
-if [ $ROLE = MASTER ]
-then
-	su - postgres -c "psql -c \"CREATE ROLE replicator LOGIN REPLICATION ENCRYPTED PASSWORD '$ODOOPASS'\""
-	su - postgres -c "psql -c \"SELECT * FROM pg_create_physical_replication_slot('replicator')\""
-	echo "host replication replicator 192.168.204.20/32 md5" >> /etc/postgresql/11/main/pg_hba.conf
-	service postgresql restart
-fi
-
-#Ejecutamos en el STANDBY
+#Ejecutamos en el STANDBY (antes de arrancar postgres)
 if [ $ROLE = STANDBY ]
 then
-	service postgresql stop
-	sleep 5
 	mv /var/lib/postgresql/11/main/ /var/lib/postgresql/11/main.bak
 	pg_basebackup -h 192.168.204.10 -U replicator -D /var/lib/postgresql/11/main/ -P --$ODOOPASS --slot replicator
 	chown -R postgres:postgres /var/lib/postgresql/11/main
@@ -33,7 +21,22 @@ then
 	echo "primary_slot_name = 'replicator'" >> /var/lib/postgresql/11/main/recovery.conf
 	echo "trigger_file = '/var/lib/postgresql/11/main/failover.trigger'" >> /var/lib/postgresql/11/main/recovery.conf
 	chown postgres:postgres /var/lib/postgresql/11/main/recovery.conf
-	service postgresql start
+fi
+
+#Arrancamos postgres
+service postgresql start
+
+#Ejecutamos en el MASTER
+if [ $ROLE = MASTER ]
+then
+	REPLICATOR = $(su - postgres -c "psql -c \"SELECT rolname FROM pg_catalog.pg_roles WHERE rolname = 'replicator'\"")
+	if [ ! $REPLICATOR ]
+	then
+		su - postgres -c "psql -c \"CREATE ROLE replicator LOGIN REPLICATION ENCRYPTED PASSWORD '$ODOOPASS'\""
+		su - postgres -c "psql -c \"SELECT * FROM pg_create_physical_replication_slot('replicator')\""
+		echo "host replication replicator 192.168.204.20/32 md5" >> /etc/postgresql/11/main/pg_hba.conf
+		service postgresql restart
+	fi
 fi
 
 #Crea el usuario odoo en postgres
