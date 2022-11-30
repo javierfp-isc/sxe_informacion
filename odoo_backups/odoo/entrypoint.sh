285aa975b1dbc912dd6beb6227cb57e5259ac1d9
Javier Fernández Peón <javierfp@iessanclemente.net>
2022-11-30 12:01:10 +0100

    permisos

--- odoo_backups/odoo/entrypoint.sh
+++ odoo_backups/odoo/entrypoint.sh
@@ -20,6 +20,9 @@ echo "127.0.0.1 postgres" >> /etc/hosts
 #Sustituimos las variables de entorno en el odoo.sh
 sed -i "s/OCBDIR/$OCBDIR/" /etc/init.d/odoo.sh
 
+#Cambiamos propietario del directorio de odoo
+[ $(stat -c "%G" $LOCALFS) == "odoo" ] || chown -R odoo:odoo /opt/odoo
+
 #Arranca odoo (while: la primera vez no arranca bien)
 while [[ ! $(service odoo.sh start) ]];do continue;done
 
